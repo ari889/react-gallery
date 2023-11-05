67051f1174e0bb1bb78a69ef7a3c1d65b05a46a5
Arijit Banarjee <ontikdev.2@gmail.com>
2023-11-05 15:37:48 +0600

    Server host change

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '192.168.68.124', // Use '0.0.0.0' to bind to all available network interfaces
-    port: 3000, // Set the port number to your preference
-  },
 })
