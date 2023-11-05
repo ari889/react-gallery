f3aace1476d57524b07a149c7980e4d727e4388c
Arijit Banarjee <ontikdev.2@gmail.com>
2023-11-05 15:02:49 +0600

    Enable touch event

--- src/components/gallery/GalleryImages.jsx
+++ src/components/gallery/GalleryImages.jsx
@@ -3,6 +3,7 @@
  */
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 
 /**
  * internal imports
@@ -11,11 +12,17 @@ import { images } from "../../App";
 import AddNewImage from "./AddNewImage";
 import GalleryImage from "./GalleryImage";
 
+/**
+ * set the backend for mobile and desktop for DndProvider
+ */
+const isTouchDevice = "ontouchstart" in document.documentElement;
+const backend = isTouchDevice ? TouchBackend : HTML5Backend;
+
 const GalleryImages = () => {
   return (
     <div>
       {/* react dnd provider start */}
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={backend}>
         {/* images grid */}
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 p-3 transition-all">
           {images?.value?.data
