7e45a6e2411e4fca67da7fd87f3a62bcc94f54b2
Arijit Banarjee <ontikdev.2@gmail.com>
2023-11-05 13:06:12 +0600

    New Comment added

--- src/components/gallery/GalleryImages.jsx
+++ src/components/gallery/GalleryImages.jsx
@@ -14,16 +14,21 @@ import GalleryImage from "./GalleryImage";
 const GalleryImages = () => {
   return (
     <div>
+      {/* react dnd provider start */}
       <DndProvider backend={HTML5Backend}>
+        {/* images grid */}
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 p-3 transition-all">
           {images?.value?.data
             .sort((a, b) => a.order - b.order)
             .map((item) => (
               <GalleryImage key={item.id} item={item} />
             ))}
+
+          {/* add new images */}
           <AddNewImage />
         </div>
       </DndProvider>
+      {/* react dnd provider end */}
     </div>
   );
 };
